173c8ba4d3c0488c032ab3ef89d011e3c659a4b4
myouhi <115806912+myouhi@users.noreply.github.com>
2025-09-17 13:32:08 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 #================================================================
-# Snell Server 管理脚本 (V29 - 终极文件修改修复版)
-#
-# 更新日志:
-# - 重写“修改配置”功能，使用更可靠的“临时文件”方法替换 sed -i，
-#   以在所有系统环境（特别是Serv00）下确保文件修改成功。
+# Snell Server 管理脚本 (serv00专用版V3版本 ）V4和v5不能使用
+# 在 Serv00 平台，您需要先手动获取一个端口。
+# 请按照以下步骤操作：
+# 1. 在浏览器中打开并登录您的 Serv00 控制面板。
+# 2. 在左侧菜单中找到 'Porty' (Ports) 选项并点击进入。
+# 3. 点击 'Add port' 按钮，创建tcp端口号。
+# 4. 运行脚本
+# 5. bash <(curl -sSL https://raw.githubusercontent.com/myouhi/serv00-snell/refs/heads/master/snell.sh)
+
+# 管理脚本快捷命令
+# 1. 下载脚本到正确的位置
+# 2. curl -o ~/snell/snell.sh https://raw.githubusercontent.com/myouhi/serv00-snell/master/snell.sh
+# 3. 给脚本加上执行权限
+# 4. chmod +x ~/snell/snell.sh
+# 5. 运行
+# 6. source ~/.bashrc
+# 7. 然后再次尝试 snell 命令
 #================================================================
 
 # --- 全局变量定义 ---
